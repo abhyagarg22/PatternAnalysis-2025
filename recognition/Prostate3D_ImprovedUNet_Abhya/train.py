96b897c2abf3175dd9f427a1da823325447856f8
Abhya Garg <abhyagarg2211@gmail.com>
2025-10-30 19:33:01 +1000

    Updated dataset path to correct location for training on Rangpur cluster

--- recognition/Prostate3D_ImprovedUNet_Abhya/train.py
+++ recognition/Prostate3D_ImprovedUNet_Abhya/train.py
@@ -11,7 +11,7 @@ import os
 # ----------------------------
 # CONFIG
 # ----------------------------
-DATA_DIR = 'data'  # change to dataset path on Rangpur
+DATA_DIR = '/home/groups/comp3710/HipMRI_Study_open'  # change to dataset path on Rangpur
 EPOCHS = 5
 BATCH_SIZE = 1
 LR = 1e-3
